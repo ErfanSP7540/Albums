954b959da8cdad596add6539d255995377085d5d
erfanSP <erfansp@erfanSPs-Mac.local>
2018-03-05 19:07:44 +0330

    AlbumDetail component

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,19 +1,18 @@
 import React,{Component} from 'react'
 import {View , Text} from 'react-native'
 import axios from 'axios'
-
+import AlbumDetail from './AlbumDetail'
 class AlbumList extends Component{
     state = { Albums:[]};
 
     componentWillMount(){
-        console.log('componentWill Mount in AlbumsList.... ');     
-        
+
          axios.get('https://rallycoding.herokuapp.com/api/music_albums')
         .then( response => {   this.setState({ Albums : response.data })  })
         .catch(   e=>{console.log('catch');console.log(e);})
     }
     renderAlbums(){
-        return this.state.Albums.map(   item=>(<Text key={item.title} >{ item.title } </Text>) )
+        return this.state.Albums.map(   item=>(<AlbumDetail key={item.title}  album={item} />) )
     }
     render(){
            console.log(this.state)
