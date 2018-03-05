1a51f0dc8ca1f04f344cb63f09a4fd755e265cca
erfanSP <erfansp@erfanSPs-Mac.local>
2018-03-05 14:24:35 +0330

    a simple style for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,38 @@
 // importing libraries for import component
 import React from 'react'
-import ReactNative , {Text} from 'react-native'
+import ReactNative , {Text, View} from 'react-native'
 
 // make a componxtent
 
 
 const Header =()=>{
+
+    const {textStyle,ViewStyle} = styles;
+
     return (
-        <Text> Albums </Text>
+        <View style={ViewStyle}> 
+            <Text style={textStyle} > Albums </Text>
+        </View>
+        
     )
+}
+
+const styles = {
+    ViewStyle:{
+        backgroundColor:'#F8F8F8',
+        justifyContent:'center',
+        alignItems:'center',
+        height:60,
+        paddingTop:10,
+        shadowColor:'#000',
+        shadowOffset:{ width :0,height:2},
+        shadowOpacity:0.9,
+        elevation:2,
+        position:'relative'
+    },
+    textStyle:{
+        fontSize:20
+    }
 }
 
 // export this component from this file 
