9f00a12d6ddf7fccf1ff92e6d784c6a300fad693
erfanSP <erfansp@erfanSPs-Mac.local>
2018-03-05 18:33:09 +0330

    fetch data and state & setState

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,17 +1,26 @@
 import React,{Component} from 'react'
 import {View , Text} from 'react-native'
-
+import axios from 'axios'
 
 class AlbumList extends Component{
-    
+    state = { Albums:[]};
+
     componentWillMount(){
         console.log('componentWill Mount in AlbumsList.... ');     
+        
+         axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+        .then( response => {   this.setState({ Albums : response.data })  })
+        .catch(   e=>{console.log('catch');console.log(e);})
+    }
+    renderAlbums(){
+        return this.state.Albums.map(   item=>(<Text key={item.title} >{ item.title } </Text>) )
     }
     render(){
+           console.log(this.state)
         return (
 
         <View>  
-            <Text> Album List !!!! </Text>
+            {this.renderAlbums()}
         </View>
     )}
 }
